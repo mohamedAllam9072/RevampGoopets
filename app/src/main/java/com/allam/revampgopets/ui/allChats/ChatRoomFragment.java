d0f38b86a317b3508749de1550a94ba11a66b4d9
Mohamed Allam <31704834+mohamedAllam9072@users.noreply.github.com>
2022-05-23 17:28:15 +0200

    start chat app

---
+++ app/src/main/java/com/allam/revampgopets/ui/allChats/ChatRoomFragment.java
@@ -0,0 +1,20 @@
+package com.allam.revampgopets.ui.allChats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.allam.revampgopets.R;
+
+public class ChatRoomFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_room, container, false);
+    }
+}
